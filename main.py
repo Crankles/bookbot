9a53560f6ef428e690549d585320869a2c5d7b14
Crankles <mddavies73@gmail.com>
2025-08-29 17:27:17 +0100

    removed hard coded book path, now requires path in CLI

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 from stats import get_num_words, character_counter, create_report
-
-
-# print("hello world")
-
+import sys
 
 def main():
 
-    book_path = "books/frankenstein.txt"
-    with open(book_path) as f:
-        file_contents = f.read()
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    else:
+        book_path = sys.argv[1]
+
+        with open(book_path) as f:
+            file_contents = f.read()
     
-        #print(file_contents)
-        #print(get_num_words(file_contents))
-        #print(character_counter(file_contents))
         print(create_report(book_path, file_contents))
-
-
-
-
 
 main()
